329e7e9588ae1c7dc7d5bd3029252f78225a2089
faristoriqu <44657860+faristoriqu@users.noreply.github.com>
2018-11-14 14:59:34 +0700

    Add files via upload

---
+++ halaman/register/register.php
@@ -0,0 +1,58 @@
+<br>
+<div class="col-md-7 ">
+<!-- Horizontal Form -->
+  <div class="box box-info">
+
+            <!-- /.box-header -->
+            <!-- form start -->
+            <form class="form-horizontal">
+              <div class="box-body">
+                </head>
+
+  <div class="login-logo">
+    <a href="../../index2.html"><b>Register</b></a>
+  </div>
+
+
+                <div class="form-group">
+                  <label  class="col-sm-3 control-label">Username</label>
+                  <div class="col-sm-7">
+                    <input type="text" class="form-control" id="username" name="username" placeholder="username">
+                  </div>
+                </div>
+
+                <!-- select -->
+                <div class="form-group">
+                  <label class="col-sm-3 control-label">Nama guru</label>
+                  <div class="col-sm-7">
+                    
+                  <select class="form-control">
+                    <option>dheni</option>
+                    <option>alex</option>
+                     <option>faris</option>
+                      <option>kizham</option>
+                       <option>uling</option>
+                  </select>
+                  </div>
+                </div>
+
+                <div class="form-group">
+                  <label  class="col-sm-3 control-label" name="alamat">Password</label>
+
+                  <div class="col-sm-7">
+                    <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                  </div>
+                </div>
+
+        
+                
+              </div>
+              <!-- /.box-body -->
+              <div class="box-footer">
+                <button type="submit" class="btn btn-default">Cancel</button>
+                <button type="submit" class="btn btn-info pull-right">Register</button>
+              </div>
+              <!-- /.box-footer -->
+            </form>
+  </div>
+</div>
